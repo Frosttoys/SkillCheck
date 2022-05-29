b60ce3322f63e7d975d863b1004ef0019454a77f
frosttoys <dy.williams777@gmail.com>
2022-05-29 11:22:16 -0600

    Changed to Factorial, Overflows Iter[2] currently

--- src/Projects/PitoN.rs
+++ src/Projects/PitoN.rs
@@ -1,8 +1,8 @@
 //! Create a program that can generate Pi reliably up to the first 1000 digits.
+use factorial::Factorial as factor;
 
-///
 pub(crate) fn main() {
-    let pi = generate_pi(500000);
+    let pi = generate_pi(10000);
     println!("{}", pi);
 }
 
@@ -18,35 +18,28 @@ pub(crate) fn main() {
 /// 6k! / ((3k)! * (k!) ^ 3)
 ///     where k is the k_th iteration.
 /// https://en.wikipedia.org/wiki/Chudnovsky_algorithm
-fn generate_pi(precision: u32) -> f64 {
-    const L_modif: f64 = 545140134f64; //Linear term modifier
-    const X_modif: f64 = -262537412640768000f64; // Exponential Term modifier
-    let     C_term: f64 = 426880f64 * f64::ceil(f64::sqrt(10005f64));
-    let mut L_term: f64 = 13591409f64; // Linear term.
-    let mut X_term: f64 = 1.0; // Exponential Term
-    let mut M_term: f64 = 1.0; // Multinomial term
+fn generate_pi(precision: u32) -> String {
+    const L_modif: i64 = 545140134; //Linear term modifier
+    const X_modif: i64 = -262537412640768000; // Exponential Term modifier
+    let     C_term: i64 = 426880 * f64::ceil(f64::sqrt(10005f64)) as i64;
+    let mut L_term: i64 = 13591409; // Linear term.
+    let mut X_term: i64 = 1; // Exponential Term
+    let mut M_term: i64 = 0; // Multinomial term
 
     let iterations = precision / 14;
-    let partial_sum: f64 = L_term as f64;
+    let partial_sum: i64 = L_term ;
 
     for iteration in 1..iterations {
-        let M_term = factorial(6 * iteration) as f64 / (factorial(3 * iteration) * factorial(iteration)^3) as f64;
+        let M_term = (factor::factorial(&(6 * iteration)) / 
+                     (factor::factorial(&(3 * iteration)) * 
+                      factor::factorial(&(iteration^3)))) as i64; //Overflows after 2 iterations...
         let L_term = L_term + L_modif;
         let X_Term = X_term * X_modif;
-        let partial_sum = partial_sum + ((M_term * L_term) / X_term) ; 
-        if 100000 % iteration == 0 {
+        let partial_sum = partial_sum + ((M_term * L_term) / X_term);
+        if 100 % iteration == 0 {
             println!("Iteration: {}, Partial Sum: {}, pi: {}", iteration, partial_sum, C_term / partial_sum)
         }
     };
-    return C_term as f64 / partial_sum as f64;
-}
-
-fn factorial(input: u32) -> u32 {
-    let mut factorate: u128 = input as u128;
-    let mut factorial: u128 = factorate;
-    while factorate > 1 {
-        factorial = factorial + (factorate - 1); //
-        factorate -= 1;
-    };
-    factorate as u32
+    //C_term  / partial_sum;
+    return String::from("");
 }
